320d57f61d1d1f069efa1daa58e2ea56a3faa581
steveblue <steve@installationspace.com>
2015-08-23 20:15:27 -0700

    Added method to check for box intersection on Rays.

--- math/Ray.js
+++ math/Ray.js
@@ -29,30 +29,36 @@ Ray.prototype.copy = function ( ray ) {
 
 };
 
-Ray.prototype.recast = function () {
 
-		var v1 = new Vec3();
-
-		return function ( t ) {
-
-			this.origin.copy( this.at( t, v1 ) );
-
-			return this;
-
-		};
-
+Ray.prototype.intersectsPlane = function(normal, dist) {
+  var tmp = new Vec3(0,0,0);
+  var o   = new Vec3(0,0,0);
+  var n   = new Vec3(normal[0],normal[1],normal[2]);
+  var out = new Vec3(0,0,0);
+  var d   = tmp.dot(this.direction,n);
+  if (d !== 0) {
+    var t = -(o.dot(this.origin, n) + dist) / d;
+    if (t < 0) {
+      return null
+    }
+    tmp = this.direction.scale(t);
+    return this.origin.add(out, tmp);
+} else if (tmp.dot(n, this.origin) + dist === 0) {
+    return this.origin;
+  } else {
+    return null;
+  }
 };
 
-Ray.prototype.intersectSphere = function(center, radius) {
+Ray.prototype.intersectsSphere = function(center, radius) {
 
-  var tmp = new Vec3(0,0,0);
-  var c = new Vec3(center[0],center[1],center[2]);
+  var c   = new Vec3(center[0],center[1],center[2]);
   var out = new Vec3(0,0,0);
-  tmp = c.subtract(this.origin);
-
+  var tmp = c.subtract(this.origin);
   var len = tmp.dot(this.direction);
-  if (len < 0) { // sphere is behind ray
-    return null
+
+  if (len < 0) { // sphere is behind the ray
+    return null;
   }
 
   tmp.scaleAndAdd(this.origin, this.direction, len);
@@ -60,13 +66,98 @@ Ray.prototype.intersectSphere = function(center, radius) {
   var dSq = tmp.squaredDistance(c);
   var rSq = radius * radius;
   if (dSq > rSq) {
-    return null
+    return null;
   }
 
   out = this.direction.scale(len - Math.sqrt(rSq - dSq));
   return out.add(out, this.origin, out);
 
 };
+
+Ray.prototype.intersectsBox = function(center, size) {
+
+    var tmin,
+        tmax,
+        tymin,
+        tymax,
+        tzmin,
+        tzmax,
+        box,
+        out,
+        invdirx = 1 / this.direction.x,
+        invdiry = 1 / this.direction.y,
+        invdirz = 1 / this.direction.z;
+
+    box = {
+        min: {
+            x: center[0]-(size[0]/2),
+            y: center[1]-(size[1]/2),
+            z: center[2]-(size[2]/2)
+        },
+        max: {
+            x: center[0]+(size[0]/2),
+            y: center[1]+(size[1]/2),
+            z: center[2]+(size[2]/2)
+        }
+    };
+
+    if ( invdirx >= 0 ) {
+
+        tmin = ( box.min.x - this.origin.x ) * invdirx;
+        tmax = ( box.max.x - this.origin.x ) * invdirx;
+
+    } else {
+
+        tmin = ( box.max.x - this.origin.x ) * invdirx;
+        tmax = ( box.min.x - this.origin.x ) * invdirx;
+    }
+
+    if ( invdiry >= 0 ) {
+
+        tymin = ( box.min.y - this.origin.y ) * invdiry;
+        tymax = ( box.max.y - this.origin.y ) * invdiry;
+
+    } else {
+
+        tymin = ( box.max.y - this.origin.y ) * invdiry;
+        tymax = ( box.min.y - this.origin.y ) * invdiry;
+    }
+
+    if ( ( tmin > tymax ) || ( tymin > tmax ) ) return null;
+
+    // These lines also handle the case where tmin or tmax is NaN
+    // (result of 0 * Infinity). x !== x returns true if x is NaN
+
+    if ( tymin > tmin || tmin !== tmin ) tmin = tymin;
+
+    if ( tymax < tmax || tmax !== tmax ) tmax = tymax;
+
+    if ( invdirz >= 0 ) {
+
+        tzmin = ( box.min.z - this.origin.z ) * invdirz;
+        tzmax = ( box.max.z - this.origin.z ) * invdirz;
+
+    } else {
+
+        tzmin = ( box.max.z - this.origin.z ) * invdirz;
+        tzmax = ( box.min.z - this.origin.z ) * invdirz;
+    }
+
+    if ( ( tmin > tzmax ) || ( tzmin > tmax ) ) return null;
+
+    if ( tzmin > tmin || tmin !== tmin ) tmin = tzmin;
+
+    if ( tzmax < tmax || tmax !== tmax ) tmax = tzmax;
+
+    //return point closest to the ray (positive side)
+
+    if ( tmax < 0 ) return null;
+
+    out = this.direction.scale(tmin >= 0 ? tmin : tmax);
+    return out.add(out, this.origin, out);
+
+};
+
 
 Ray.prototype.equals = function ( ray ) {
 
